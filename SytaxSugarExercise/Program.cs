36a165258d22beaaaabd60b39a01221d4d0af92b
klreeves30 <klreeves30@gmail.com>
2021-10-27 12:00:27 -0400

    initial commit

---
+++ SytaxSugarExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SytaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 4;
+            ///string response;
+
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + "greater than or equal to nine";
+            //}
+            //Console.WriteLine(response);
+
+
+            var response  = (answer < 9 )? $"{answer} is less than nine" : $"{answer} greater than or equal to nine" ;
+            Console.WriteLine(response);
+        }
+    }
+}
